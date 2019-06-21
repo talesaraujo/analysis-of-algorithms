eae6bf79515a730452ad45ae5384ace7fe3f8be2
talesaraujo <talesaraujodossantos@gmail.com>
2019-06-21 01:42:01 -0300

    test huffman

---
+++ huffman.py
@@ -0,0 +1,60 @@
+
+class Node(object):
+    def __init__(self, value, parent, left, right):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def insert(self, data):
+        if data < self.value:
+        
+        else:
+
+
+
+
+    
+
+    def display(self):
+        for node in self.node_list:
+            pass
+
+
+
+
+
+
+
+encode = 'Ana ama sua nana, sua mana e banana'
+
+
+def count_sequency(text):
+"""
+    Returns a dictionary with the frequencies for each character.
+"""
+    already_counted = []
+
+    for char in text:
+        
+
+
+    pass
+
+def huffman_encoding(sequency):
+"""
+    We are given an array with characters and their proper frequencies,
+    and we need to return the Huffman coding to this sequence.
+"""
+    pass
+    
+
+
+sample_dict = {
+    'a': .5,
+    'b': .25,
+    'c': .125,
+    'd': .0625,
+    'e': .0625
+}
+
+print(sample_dict)
