d8f0b39d2ea5de76fbd20bb1058dfa8d4265ef1e
talesaraujo <talesaraujodossantos@gmail.com>
2019-06-21 02:31:19 -0300

    Count sequency

--- huffman.py
+++ huffman.py
@@ -1,4 +1,3 @@
-
 class Node(object):
     def __init__(self, value, parent, left, right):
         self.value = value
@@ -7,44 +6,44 @@ class Node(object):
 
     def insert(self, data):
         if data < self.value:
-        
+            pass
         else:
-
-
-
-
-    
+            pass
 
     def display(self):
         for node in self.node_list:
             pass
-
-
-
-
-
+        for element in text:
+            pass
 
 
 encode = 'Ana ama sua nana, sua mana e banana'
 
-
-def count_sequency(text):
-"""
-    Returns a dictionary with the frequencies for each character.
-"""
-    already_counted = []
-
+def count_frequency(text, rel=True):
+    """
+        Returns a dictionary with the frequencies for each character.
+    """
+    frequencies = {}
     for char in text:
-        
+        if char not in frequencies:
+            counter = 0
+            for ch in text:
+                if char == ch:
+                    counter += 1
+            frequencies[char] = counter
 
+    if (rel):
+        for key, value in frequencies.items():
+            frequencies[key] = value/len(text)
 
-    pass
+    return frequencies
+
 
 def huffman_encoding(sequency):
-"""
-    We are given an array with characters and their proper frequencies,
-    and we need to return the Huffman coding to this sequence.
-"""
+    """
+        We are given an array with characters and their proper frequencies,
+        and we need to return the Huffman coding to this sequence.
+    """
     pass
     
 
@@ -57,4 +56,6 @@ sample_dict = {
     'e': .0625
 }
 
-print(sample_dict)
+print(encode)
+for key, value in count_frequency(encode).items():
+    print("{}: {}".format(key, value))
